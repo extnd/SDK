6f6f902c8b152a019dae72aab490a7b5b41beb2b
Rich Waters <github@rich-waters.com>
2007-05-25 01:25:34 +0000

    Added comments and refomatted code, added init call to DominoUI's constructor, removed session class in favor of the notes form version

--- source/domino/DominoUI.js
+++ source/domino/DominoUI.js
@@ -1,4 +1,24 @@
 /* Domino Default User Interface */
+/*
+ * Ext.nd JS library Alpha 1
+ * Copyright (c) 2006-2007, ExtND
+ * licensing@extjs.com
+ * 
+ * http://www.extjs.com/license
+ */
+ 
+/**
+ * @class Ext.nd.DominoUI
+  * <p>Here's an example showing the creation of a typical DominoUI:</p> * <pre><code>
+new Ext.nd.DominoUI({
+  uiOutline : {outlineName: "mainOL"},
+  uiView : {viewName: "Requests", viewTitle: "Requests"}		
+});
+  * @cfg {Object} uiOutline A {@link Ext.nd.UIOutline} config object * @cfg {Object} uiView A {@link Ext.nd.UIView} config object
+ * @constructor
+ * Create an integrated domino interface, with a view and an outline
+ * @param {Object} config Configuration options
+ */
 Ext.nd.DominoUI = function(config) {
 
    // set defaults
@@ -39,78 +59,76 @@ Ext.nd.DominoUI = function(config) {
    Ext.apply(this,config);
 
    // init the DominoUI
-   //this.init(); // user calls this -or- should we???
+   this.init();
    
 }; // end Ext.nd.DominoUI
-  
-Ext.nd.DominoUI.prototype.init = function() {
-      
-   // create the UI
-   // creates outlinePanel, viewPanel, statusPanel
-   this.createDominoUI();
-   
-   // if we have a viewName or a viewUrl we can create the view
-   if (this.uiView.viewName != '' || this.uiView.viewUrl != '') {
-      this.uiView = new Ext.nd.UIView(Ext.apply({
-         container : this.viewPanel,
-         layout : this.layout,
-         statusPanel : this.statusPanel
-      }, this.uiView));
 
+Ext.nd.DominoUI.prototype = {
+  init: function() {
+    // create the UI
+    // creates outlinePanel, viewPanel, statusPanel
+    this.createDominoUI();
+    
+    // if we have a viewName or a viewUrl we can create the view
+    if (this.uiView.viewName != '' || this.uiView.viewUrl != '') {
+      this.uiView = new Ext.nd.UIView(Ext.apply({
+        container : this.viewPanel,
+        layout : this.layout,
+        statusPanel : this.statusPanel
+      }, this.uiView));
+ 
       // set the title of the view panel
       this.viewTitle = (this.viewTitle) ? this.viewTitle : (this.uiView.viewName) ? this.uiView.viewName : this.uiView.viewUrl;
       this.viewPanel.setTitle(this.viewTitle);
-   }
-   
-   // if we have a outlineName or a outlineUrl we can create the outline
-   if (this.uiOutline.outlineName != '' || this.uiOutline.outlineUrl != '') {
+    }
+    
+    // if we have a outlineName or a outlineUrl we can create the outline
+    if (this.uiOutline.outlineName != '' || this.uiOutline.outlineUrl != '') {
       this.uiOutline = new Ext.nd.UIOutline(Ext.apply({
-         layout : this.layout,
-         outlinePanel : this.outlinePanel, 
-         viewPanel : this.viewPanel,
-         uiView : this.uiView
+        layout : this.layout,
+        outlinePanel : this.outlinePanel, 
+        viewPanel : this.viewPanel,
+        uiView : this.uiView
       },this.uiOutline));
-   }
-   
-}; // end init()
-      
-Ext.nd.DominoUI.prototype.createDominoUI = function() {
-   this.layout = new Ext.BorderLayout(document.body, {
-      hideOnLayout: true,
-      west: this.west,
-      center: this.center,
-      south: this.south
-   });
+    }
+  },
+  
+  // Private
+  createDominoUI: function() {
+    this.layout = new Ext.BorderLayout(document.body, {
+        hideOnLayout: true,
+        west: this.west,
+        center: this.center,
+        south: this.south
+     });
+  
+     this.layout.beginUpdate();
+  
+     //initialize the statusbar
+     this.statusPanel = this.layout.add('south', new Ext.ContentPanel('extnd-status', {
+        autoCreate : true
+     }));
+  
+     // create outline panel
+     this.outlinePanel = this.layout.add('west', new Ext.ContentPanel('extnd-outline', {
+        autoCreate : true,
+        title : document.title,
+        fitToFrame : true
+     }));
+     
+     // crate view panel
+     this.viewPanel = this.layout.add('center', new Ext.ContentPanel('extnd-view', {
+        autoCreate : true,
+        title : 'View',
+        closable : false,
+        fitToFrame : true
+     }));
+     
+     // tell the layout we are done so it can draw itself on the screen
+     this.layout.endUpdate();
+  },
 
-   this.layout.beginUpdate();
-
-   //initialize the statusbar
-   this.statusPanel = this.layout.add('south', new Ext.ContentPanel('extnd-status', {
-      autoCreate : true
-   }));
-
-   // create outline panel
-   this.outlinePanel = this.layout.add('west', new Ext.ContentPanel('extnd-outline', {
-      autoCreate : true,
-      title : document.title,
-      fitToFrame : true
-   }));
-   
-   // crate view panel
-   this.viewPanel = this.layout.add('center', new Ext.ContentPanel('extnd-view', {
-      autoCreate : true,
-      title : 'View',
-      closable : false,
-      fitToFrame : true
-   }));
-   
-   // tell the layout we are done so it can draw itself on the screen
-   this.layout.endUpdate();
-
+  showError: function() {
+    alert("An error has occured");
+  }
 };
-
-Ext.nd.DominoUI.prototype.showError = function(){
-   alert("an error occurred");
-};
-
-
