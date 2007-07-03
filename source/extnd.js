e945bce84fdacbeb9cd458228a7d33ec48f0ac74
Jack Ratcliff <jack.ratcliff@gmail.com>
2007-07-03 00:29:39 +0000

    moved the Ext.namespace code to the top of the file so that the Ext.nd namespaces could be properly defined.  We had been depending on the Ext.nd.Session.js form to do this but we want this code, instead, to be here.

--- source/extnd.js
+++ source/extnd.js
@@ -1,3 +1,13 @@
+/*
+ * Ext.nd JS library 1.0
+ * Copyright (c) 2006-2007, ExtND
+ * licensing@extjs.com
+ * 
+ * http://www.extjs.com/license
+ */
+
+Ext.namespace("Ext.nd", "Ext.nd.grid", "Ext.nd.data", "Ext.nd.domino");
+
 Ext.nd.extndUrl = "/extnd.nsf/";  // default
 
 Ext.nd.getBlankImageUrl = function() {
@@ -5,8 +15,6 @@ Ext.nd.getBlankImageUrl = function() {
 };
 
 Ext.nd.init = function(config) {
-	Ext.apply(this,config);
-	Ext.BLANK_IMAGE_URL = this.getBlankImageUrl();	
+   Ext.apply(this,config);
+   Ext.BLANK_IMAGE_URL = this.getBlankImageUrl();  
 };
-
-Ext.namespace("Ext.nd", "Ext.nd.grid", "Ext.nd.data", "Ext.nd.domino");
