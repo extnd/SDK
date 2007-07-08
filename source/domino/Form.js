7072b6bc5417c02d4e4ebefdda715cdb8a2131b3
Jack Ratcliff <jack.ratcliff@gmail.com>
2007-07-08 13:03:25 +0000

    changed to 2 space tabs and changed a for loop that loops through class names.

--- source/domino/Form.js
+++ source/domino/Form.js
@@ -6,23 +6,23 @@
  */
 Ext.nd.Form = function(config) {
    
-   var sess = Ext.nd.Session; 
-   var db = sess.CurrentDatabase;
-   var uidoc = Ext.nd.UIDocument;
+  var sess = Ext.nd.Session; 
+  var db = sess.CurrentDatabase;
+  var uidoc = Ext.nd.UIDocument;
    
-   // defaults
-   this.dbPath = db.WebFilePath;
-   this.showActionbar = true;
-   this.toolbar = false;
-   this.bConvertFields = true;
-   this.form = uidoc.form;
-   this.formName = uidoc.formName;
+  // defaults
+  this.dbPath = db.WebFilePath;
+  this.showActionbar = true;
+  this.toolbar = false;
+  this.bConvertFields = true;
+  this.form = uidoc.form;
+  this.formName = uidoc.formName;
 
-   // Set any config params passed in to override defaults
-   Ext.apply(this,config);
+  // Set any config params passed in to override defaults
+  Ext.apply(this,config);
          
-   // formUrl is either passed in or built from dbPath and formName
-   this.formUrl = (this.formUrl) ? this.formUrl : this.dbPath + this.formName;
+  // formUrl is either passed in or built from dbPath and formName
+  this.formUrl = (this.formUrl) ? this.formUrl : this.dbPath + this.formName;
      
 };
 
@@ -47,19 +47,19 @@ Ext.nd.Form.prototype = {
     // need an actionbar?
     if (this.showActionbar || this.toolbar) {
       if (!this.toolbar) {
-         var tb = dh.append(ui,{tag: 'div'});
-         this.toolbar = new Ext.nd.Actionbar({
-            container:tb, 
-            noteType:'form', 
-            noteName:this.formName
-         });
+        var tb = dh.append(ui,{tag: 'div'});
+        this.toolbar = new Ext.nd.Actionbar({
+          container:tb, 
+          noteType:'form', 
+          noteName:this.formName
+        });
       }
     } 
 
     // define the layout
     var layout = new Ext.BorderLayout(document.body, {
-       north : {titlebar: false},
-       center: { }
+      north : {titlebar: false},
+      center: { }
     });
    
     layout.beginUpdate();
@@ -83,62 +83,86 @@ Ext.nd.Form.prototype = {
       var converted;
       
       for (var i=0; i<xndElements.length; i++) {
-         converted = false;
-         el = xndElements[i];
-         arClasses = el.className.split(' ');
+        converted = false;
+        el = xndElements[i];
+        arClasses = el.className.split(' ');
 
-         for (var c=0; c < arClasses.length; c++) {
-            cls = arClasses[c];
+        // check classes first
+        for (var c=0; c < arClasses.length; c++) {
+          cls = arClasses[c];
    
-            switch (cls) {
-               case 'xnd-combobox' :
-                  var cb = new Ext.form.ComboBox({
-                     typeAhead : true,
-                     triggerAction : 'all',
-                     transform : el,
-                     forceSelection : true
-                  });
-                  converted = true;
-                  break;
+          switch (cls) {
+            case 'xnd-combobox' :
+              var cb = new Ext.form.ComboBox({
+                typeAhead : true,
+                triggerAction : 'all',
+                transform : el,
+                forceSelection : true
+              });
+              converted = true;
+              break;
 
-               case 'xnd-date' :
-                  var dt = new Ext.form.DateField({
-                     selectOnFocus : true
-                  });      
-                  dt.applyTo(el);
-                  converted = true;
-                  break;
+            case 'xnd-date' :
+              var dt = new Ext.form.DateField({
+                selectOnFocus : true
+              });      
+              dt.applyTo(el);
+              converted = true;
+              break;
                
-               default :
-                  break;               
-            } // end switch(cls)
+            default :
+              break;               
+          } // end switch(cls)
 
-            if (!converted) {
-               switch(el.tagName) {
-                     case 'SELECT' :
-                     var cb = new Ext.form.ComboBox({
-                        typeAhead : true,
-                        triggerAction : 'all',
-                        transform : el,
-                        forceSelection : true
-                     });
-                     break;
-                  
-                     case 'TEXTAREA' :
-                     var ta = new Ext.form.TextArea();
-                     ta.applyTo(el);
-                     break;
+        } // end for arClasses
 
-                     default :                  
-                        var f = new Ext.form.Field();
-                        f.applyTo(el);
-                     break;
-               } // end switch(el.tagName)
-            } // end !converted
+        // if the field did not have a 'xnd-' class then try to convert it based off of the tagName or type
+        if (!converted) {
+          switch(el.tagName) {
+            case 'SELECT' :
+              var cb = new Ext.form.ComboBox({
+                typeAhead : true,
+                triggerAction : 'all',
+                transform : el,
+                forceSelection : true
+              });
+              break;
+
+            case 'TEXTAREA' :
+              var ta = new Ext.form.TextArea();
+              ta.applyTo(el);
+              break;
+
+
+            case 'INPUT' :
+              var type = el.getAttribute('type');
+              switch(type) {
+                case 'hidden' :
+                  break;
+                case 'checkbox' :
+                  //var ckb = new Ext.form.Checkbox();
+                  //ckb.applyTo(el);
+                  break;
+                default :
+                  var f = new Ext.form.Field();
+                  f.applyTo(el);
+                  break;
+              
+              } // end switch(type)
+              
+            default :                  
+              var f = new Ext.form.Field();
+              f.applyTo(el);
+              break;
+              
+          } // end switch(el.tagName)
+          
+        } // end !converted
    
-         } // end for arClasses
+         
       } // end for xndElements
-   } // if this.bConvertFields
+      
+    } // if this.bConvertFields
 
   } 
 
