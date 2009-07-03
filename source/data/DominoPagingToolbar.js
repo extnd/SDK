1a6cc77d0f2cb9a9d6b7dc8ca8c0f1cedb30ec16
Jack Ratcliff <jack.ratcliff@gmail.com>
2009-07-03 06:08:44 +0000

    - basically had to do a rewrite since Ext 3rc3 replaced the onClick method with several methods to handle the clicking of the first, previous, next, last, and refresh buttons. - also had to come up with a way to fix the inputItem field since it used to be a text field but in Ext 3rc3 it was changed to a number field but with domino, we need a text field so we can enter view hierarchy start parms such as 2.1.3.2, etc.

--- source/data/DominoPagingToolbar.js
+++ source/data/DominoPagingToolbar.js
@@ -13,188 +13,219 @@ Ext.nd.DominoPagingToolbar = function(config){
 
 Ext.extend(Ext.nd.DominoPagingToolbar, Ext.PagingToolbar, {
 
-  // change the displayed text
-  beforePageText : "Showing entries ",
-  afterPageText : " - {0}",
-  middlePageText: " of ",
-  
-  initComponent : function() {
-    this.previousCursor = 1;
-    this.previousStart = [];
-    this.previousStartMC = new Ext.util.MixedCollection();
-    Ext.nd.DominoPagingToolbar.superclass.initComponent.call(this);
-  },
-  
-  // private override since pageNum could represent a deeply nested domino heirarchy (ie 3.22.1.2)
-  // and the normal Ext pageNum expects a number
-  readPage : function(d){
-      var pageNum = this.field.value;    
-      if (!pageNum) {
-          this.field.value = d.activePage;
-          return false;
-      }
-      return pageNum;
-  },
+	// change the displayed text
+	beforePageText : "Showing entries ",
+	afterPageText : " - {0}",
+	middlePageText: " of ",
 
-  // private
-  onPagingKeydown : function(e){
-      var k = e.getKey(), d = this.getPageData(), pageNum;
-      if (k == e.RETURN) {
-          e.stopEvent();
-          pageNum = this.readPage(d);
-          this.doLoad(pageNum);
-      }else if (k == e.HOME || k == e.END){
-          e.stopEvent();
-          pageNum = k == e.HOME ? 1 : d.pages;
-          this.field.value = pageNum;
-      }else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN){
-          e.stopEvent();
-          if(pageNum == this.readPage(d)){
-              var increment = e.shiftKey ? 10 : 1;
-              if(k == e.DOWN || k == e.PAGEDOWN){
-                  increment *= -1;
-              }
-              pageNum += increment;
-              if(pageNum >= 1 & pageNum <= d.pages){
-                  this.field.value = pageNum;
-              }
-          }
-      }
-  },
+	initComponent : function() {
 
-  // private
-  onClick : function(button){
-    var store = this.store;
-    var d = this.getPageData();
-    var start;
-    this.button = button;
+		Ext.nd.DominoPagingToolbar.superclass.initComponent.call(this);
     
-    switch(button){
-      case this.first:
-        store.load({params: Ext.apply(store.lastOptions.params, {start: 1,count: this.pageSize})});
-        break;
-      case this.prev:
-        var first = store.data.first();
+		/* 	starting with Ext 3rc3 the inputItem was changed from a text field
+		 *	to a number field so we need to change it back so that it will work
+		 *	with Domino's hierarchical start params (i.e. 2.3.2.1, etc.) 
+		 */
+		Ext.each(this.items.items, function(item, index, allItems){
+			if (item.getXType && item.getXType() == 'numberfield'){
+				allItems[index] = this.inputItem = new Ext.form.TextField({
+					cls: 'x-tbar-page-number',
+					enableKeyEvents: true,
+					selectOnFocus: true,
+					grow: true,
+					listeners: {
+		            	scope: this,
+		            	keydown: this.onPagingKeyDown,
+		            	blur: this.onPagingBlur
+		        	}
+				});
+			}
+		}, this)
+
+		this.previousCursor = 1;
+		this.previousStart = [];
+		this.previousStartMC = new Ext.util.MixedCollection();
+    
+	},
+
+	// private override since pageNum could represent a deeply nested domino heirarchy (ie 3.22.1.2)
+	// and the normal Ext pageNum expects a number
+	readPage : function(d){
+		var pageNum = this.inputItem.getValue();    
+		if (!pageNum) {
+			this.inputItem.setValue(d.activePage);
+			return false;
+		}
+		return pageNum;
+	},
+
+	// private override since pageNum could represent a deeply nested domino heirarchy (ie 3.22.1.2)
+	// and the normal Ext pageNum expects a number
+	onPagingKeyDown : function(field, e){
+		var k = e.getKey(), d = this.getPageData(), pageNum;
+		if (k == e.RETURN) {
+			e.stopEvent();
+			pageNum = this.readPage(d);
+			this.doLoad(pageNum);
+		}else if (k == e.HOME || k == e.END){
+			e.stopEvent();
+			pageNum = k == e.HOME ? 1 : d.pages;
+			field.setValue(pageNum);
+		}else if (k == e.UP || k == e.PAGEUP || k == e.DOWN || k == e.PAGEDOWN){
+			e.stopEvent();
+			if(pageNum == this.readPage(d)){
+				var increment = e.shiftKey ? 10 : 1;
+				if(k == e.DOWN || k == e.PAGEDOWN){
+					increment *= -1;
+				}
+				pageNum += increment;
+				if(pageNum >= 1 & pageNum <= d.pages){
+					field.setValue(pageNum);
+				}
+			}
+		}
+	},
+
+	// private
+	onFirstClick : function(){
+		this.prevButton = 'first';
+		//this.store.load({params: Ext.apply(this.store.lastOptions.params, {start: 1,count: this.pageSize})});
+		this.doLoad(1);
+	},
+
+
+	onPrevClick : function(){
+		this.prevButton = 'previous';
+		var start;
+        var first = this.store.data.first();
         var firstPosition = first.node.attributes.getNamedItem('position').value;
         // if the previous page exists in cache, use it
         var indexFirst = this.previousStartMC.indexOfKey(firstPosition)
         if (indexFirst != -1) {
-          if (indexFirst == 0) {
-            start = 1;
-          } else {
-            start = this.previousStartMC.get(indexFirst-1);
-          }
+        	if (indexFirst == 0) {
+        		start = 1;
+        	} else {
+        		start = this.previousStartMC.get(indexFirst-1);
+        	}
         } else {
-          if (this.prevButton == 'last') {
-            start = this.previousStartMC.last();
-          } else {
-            start = 1;
-            this.previousStartMC.clear(); // clear everything and start over
-          }
+        	if (this.prevButton == 'last') {
+        		start = this.previousStartMC.last();
+        	} else {
+        		start = 1;
+        		this.previousStartMC.clear(); // clear everything and start over
+        	}
         }
-        store.load({params: Ext.apply(store.lastOptions.params, {start: start,count: this.pageSize})});
-        break;
-      case this.next:
-        var last = store.data.last();
-        var lastIndex = store.data.indexOf(last);
-        if (store.data.length > 0) {
-          if (last.isCategoryTotal) {
-            var previous = store.getAt(lastIndex-1);
-            start = previous.node.attributes.getNamedItem('position').value;
-          } else {
-            start = last.node.attributes.getNamedItem('position').value;
-          }
-          this.previousStartMC.add(start,start);
-        } else {
-          start = 1;
-        }
-        store.load({params: Ext.apply(store.lastOptions.params, {start: start,count: this.pageSize})});
-        break;
-      case this.last:
-        var total = store.getTotalCount();
+        //this.store.load({params: Ext.apply(this.store.lastOptions.params, {start: start,count: this.pageSize})});
+        this.doLoad(start);
+	},
+
+	onNextClick : function(){
+    	this.prevButton = 'next';
+		var start;
+		var last = this.store.data.last();
+		var lastIndex = this.store.data.indexOf(last);
+		if (this.store.data.length > 0) {
+			if (last.isCategoryTotal) {
+				var previous = this.store.getAt(lastIndex-1);
+				start = previous.node.attributes.getNamedItem('position').value;
+			} else {
+				start = last.node.attributes.getNamedItem('position').value;
+			}
+			this.previousStartMC.add(start,start);
+		} else {
+			start = 1;
+		}
+		//this.store.load({params: Ext.apply(this.store.lastOptions.params, {start: start,count: this.pageSize})});
+		this.doLoad(start);
+	},
+
+	onLastClick : function(){
+      	this.prevButton = 'last';
+      	var start;
+        var total = this.store.getTotalCount();
         var extra = total % this.pageSize;
-        var lastStart = this.isCategorized ? total : extra ? (total - extra) : total-this.pageSize;
-        store.load({params: Ext.apply(store.lastOptions.params, {start: lastStart, count: this.pageSize})});
-        break;
-      case this.refresh:
-        store.load({params: Ext.apply(store.lastOptions.params, {start: this.cursor, count: this.pageSize})});
-        break;
-  
-    }
-    // capture the 'button' to check for later
-    this.prevButton = button;
-  },
+        start = this.isCategorized ? total : extra ? (total - extra) : total-this.pageSize;
+        //this.store.load({params: Ext.apply(this.store.lastOptions.params, {start: start, count: this.pageSize})});
+        this.doLoad(start);
+	},
+
+	onRefreshClick : function(){
+      	this.prevButton = 'refresh';
+        //this.store.load({params: Ext.apply(this.store.lastOptions.params, {start: this.cursor, count: this.pageSize})});
+      	this.doLoad(this.cursor);
+	},
 
   // private override to deal with domino's categories and views with reader/author fields
-  onLoad : function(store, r, o){
-    this.cursor = o.params ? (o.params.start ? o.params.start : 1) : 1;
-    var d = this.getPageData(), ap = d.activePage, ps = d.pages;
-    
-    // reset activePage if no start param
-    // start param is removed when user clicks on a column to resort
-    // this is so that the paging will start over since we are taking the user back to the top of the view (sorted by the column they clicked)
-    if (o.params) {
-        if (!o.params.start) {
-            d.activePage = 1;
-            ap = 1;
-        }
-    } else {
-        d.activePage = 1;
-        ap = 1;        
-    }
-    
-    // resize the text field to hold the starting entry value
-    var tm = Ext.util.TextMetrics.createInstance(this.field,100);
-    this.inputItem.el.applyStyles({'width':Math.max(tm.getWidth(ap)+10,20), 'textAlign' : 'right'});
- 
-    this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
-    this.field.value = ap;
-        
-    // the Ext.nd way that works for categorized views and views with reader fields
-    this.first.setDisabled(store.baseParams.start == 1);
-    this.prev.setDisabled(store.baseParams.start == 1);
-    this.next.setDisabled(store.data.length < store.baseParams.count); 
-    this.last.setDisabled(store.data.length < store.baseParams.count);
-    
-    this.refresh.enable();
-    
-    this.fireEvent('change', this, d);
-  },
+	onLoad : function(store, r, o){
+		this.cursor = o.params ? (o.params.start ? o.params.start : 1) : 1;
+		var d = this.getPageData(), ap = d.activePage, ps = d.pages;
 
-  // private
-  getPageData : function(){
-    var total = this.store.getTotalCount();
-    var activePage, first, firstText, last, lastText, previous;
-    
-    // reset this.button
-    this.button = "";
+		// reset activePage if no start param
+		// start param is removed when user clicks on a column to resort
+		// this is so that the paging will start over since we are taking the user back to the top of the view (sorted by the column they clicked)
+		if (o.params) {
+			if (!o.params.start) {
+				d.activePage = 1;
+				ap = 1;
+			}
+		} else {
+			d.activePage = 1;
+			ap = 1;        
+		}
 
-    // for the new way of showing where within a view you are
-    if (this.store.data.length > 0) {
-      first = this.store.data.first();
-      firstText = first.node.attributes.getNamedItem('position').value;
-      last = this.store.data.last();
-      if (last.isCategoryTotal) {
-        var lastIndex = this.store.data.indexOf(last);
-        if (lastIndex == 0) {
-            lastText = firstText;
-        } else {
-            previous = this.store.getAt(lastIndex-1);
-            lastText = previous.node.attributes.getNamedItem('position').value;    
-        }
-      } else {
-        lastText = last.node.attributes.getNamedItem('position').value;
-      }
-    } else {
-      firstText = "1";
-      lastText = "1";
-    }
-    
-    return {
-      total : total,
-      activePage : firstText,
-      pages : lastText + this.middlePageText + total
-    };
-  }
-  
+		// resize the text field to hold the starting entry value
+		//var tm = Ext.util.TextMetrics.createInstance(this.inputItem.el.dom,100);
+		//this.inputItem.el.applyStyles({'width':Math.max(tm.getWidth(ap)+10,20), 'textAlign' : 'right'});
+
+		this.afterTextItem.setText(String.format(this.afterPageText, d.pages));
+		this.inputItem.setValue(ap);
+
+		// 	the Ext.nd way that works for categorized views and views with reader fields
+		this.first.setDisabled(store.baseParams.start == 1);
+		this.prev.setDisabled(store.baseParams.start == 1);
+		this.next.setDisabled(store.data.length < store.baseParams.count); 
+		this.last.setDisabled(store.data.length < store.baseParams.count);
+
+		this.refresh.enable();
+
+		this.fireEvent('change', this, d);
+	},
+
+	// private
+	getPageData : function(){
+		var total = this.store.getTotalCount();
+		var activePage, first, firstText, last, lastText, previous;
+
+		// reset this.button
+		this.button = "";
+
+		// for the new way of showing where within a view you are
+		if (this.store.data.length > 0) {
+			first = this.store.data.first();
+			firstText = first.node.attributes.getNamedItem('position').value;
+			last = this.store.data.last();
+			if (last.isCategoryTotal) {
+				var lastIndex = this.store.data.indexOf(last);
+				if (lastIndex == 0) {
+					lastText = firstText;
+				} else {
+					previous = this.store.getAt(lastIndex-1);
+					lastText = previous.node.attributes.getNamedItem('position').value;    
+				}
+			} else {
+				lastText = last.node.attributes.getNamedItem('position').value;
+			}
+		} else {
+			firstText = "1";
+			lastText = "1";
+		}
+
+		return {
+			total : total,
+			activePage : firstText,
+			pages : lastText + this.middlePageText + total
+		};
+		
+	} // eo getPageData
+
 });
