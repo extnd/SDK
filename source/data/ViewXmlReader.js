c8d1e24d72015e47663cd33ebb9ee2f91a62c182
Jack Ratcliff <jack.ratcliff@gmail.com>
2009-08-02 01:24:33 +0000

    - added a default to the meta param so that ViewXmlReader can be used without having to pass in a value for the 'meta' parameter.
    - changed the getViewColumnValue method so that it does not loop through ALL columns when looking for the correct column to get value(s) for.  Also changed this method to allow someone to pass in their own xpath 'map' if desired.

--- source/data/ViewXmlReader.js
+++ source/data/ViewXmlReader.js
@@ -1,178 +1,223 @@
 /**
  * @class Ext.nd.data.ViewXmlReader
- * @extends Ext.data.XmlReader
- * An expanded version of Ext's XmlReader to deal with Domino's funky ReadViewEntries format
+ * @extends Ext.data.XmlReader An expanded version of Ext's XmlReader to deal
+ *          with Domino's funky ReadViewEntries format
  * @cfg {String} totalRecords - override domino's toplevelentries
- * @cfg {String} record The DomQuery path to the repeated element which contains record information.
- * @cfg {String} success The DomQuery path to the success attribute used by forms.
- * @cfg {String} id The DomQuery path relative from the record element to the element that contains
- * a record identifier value.
- * @constructor
- * Create a new XmlReader
- * @param {Object} meta Metadata configuration options
- * @param {Mixed} recordType The definition of the data record type to produce.  This can be either a valid
- * Record subclass created with {@link Ext.data.Record#create}, or an array of objects with which to call
- * Ext.data.Record.create.  See the {@link Ext.data.Record} class for more details.
+ * @cfg {String} record The DomQuery path to the repeated element which contains
+ *      record information.
+ * @cfg {String} success The DomQuery path to the success attribute used by
+ *      forms.
+ * @cfg {String} id The DomQuery path relative from the record element to the
+ *      element that contains a record identifier value.
+ * @constructor Create a new XmlReader
+ * @param {Object}
+ *            meta Metadata configuration options
+ * @param {Mixed}
+ *            recordType The definition of the data record type to produce. This
+ *            can be either a valid Record subclass created with
+ *            {@link Ext.data.Record#create}, or an array of objects with which
+ *            to call Ext.data.Record.create. See the {@link Ext.data.Record}
+ *            class for more details.
  */
 Ext.nd.data.ViewXmlReader = function(meta, recordType) {
-  Ext.nd.data.ViewXmlReader.superclass.constructor.call(this, meta, recordType);
+    meta = meta || {};
+    
+	meta = Ext.apply({
+		root : 'viewentries',
+		record : 'viewentry',
+		totalRecords : '@toplevelentries',
+		id : '@position'
+	   }, meta);
+
+	Ext.nd.data.ViewXmlReader.superclass.constructor.call(this, meta, recordType || meta.fields);
 };
 
 Ext.extend(Ext.nd.data.ViewXmlReader, Ext.data.XmlReader, {
-  /**
-   * Create a data block containing Ext.data.Records from an XML document.
-   * @param {Object} doc A parsed XML document.
-   * @return {Object} records A data block which is used by an {@link Ext.nd.ViewStore} as
-   * a cache of Ext.data.Records.
-   */
-  readRecords : function(doc) {
-    /**
-     * After any data loads/reads, the raw XML Document is available for further custom processing.
-     * @type XMLDocument
-     */
-    this.xmlData = doc;
-    var root = doc.documentElement || doc;
-    var q = Ext.DomQuery;
-    var recordType = this.recordType, fields = recordType.prototype.fields;
-    var sid = this.meta.id;
-    var totalRecords = 0;
-    if (this.meta.totalRecords) {
-      totalRecords = q.selectNumber(this.meta.totalRecords, root, 0);
-    }
-    var records = [];
-    var ns = q.select(this.meta.record, root);
-    for (var i = 0, len = ns.length; i < len; i++) {
-      var n = ns[i];
-      var values = {};
-      var metadata = {};
-      var id = sid ? q.selectValue(sid, n) : undefined;
-      for (var j = 0, jlen = fields.length; j < jlen; j++) {
-        var f = fields.items[j];
-        //var v = q.selectValue(f.mapping || f.name, n, f.defaultValue);
-        // we use '.mapping' since it is the columnnumber and '.name' may not have a value
-        var valuePlusDominoMetaData = this.getViewColumnValue(n, f.mapping, f.defaultValue);
-        var v = valuePlusDominoMetaData.data;
-        delete valuePlusDominoMetaData.data;
-        metadata[f.name] = valuePlusDominoMetaData;
+	/**
+	 * Create a data block containing Ext.data.Records from an XML document.
+	 * 
+	 * @param {Object}
+	 *            doc A parsed XML document.
+	 * @return {Object} records A data block which is used by an
+	 *         {@link Ext.nd.ViewStore} as a cache of Ext.data.Records.
+	 */
+	readRecords : function(doc) {
+		/**
+		 * After any data loads/reads, the raw XML Document is available for
+		 * further custom processing.
+		 * 
+		 * @type XMLDocument
+		 */
+		this.xmlData = doc;
+		var root = doc.documentElement || doc;
+		var q = Ext.DomQuery;
+		var recordType = this.recordType, fields = recordType.prototype.fields;
+		var sid = this.meta.id;
+		var totalRecords = 0;
+		if (this.meta.totalRecords) {
+			totalRecords = q.selectNumber(this.meta.totalRecords, root, 0);
+		}
+		var records = [];
+		var ns = q.select(this.meta.record, root);
+		for (var i = 0, len = ns.length; i < len; i++) {
+			var n = ns[i];
+			var values = {};
+			var metadata = {};
+			var id = sid ? q.selectValue(sid, n) : undefined;
+			for (var j = 0, jlen = fields.length; j < jlen; j++) {
+				var f = fields.items[j];
+				// var v = q.selectValue(f.mapping || f.name, n,
+				// f.defaultValue);
+				// we use '.mapping' since it is the columnnumber and '.name'
+				// may not have a value
+				var valuePlusDominoMetaData = this.getViewColumnValue(n,
+						f.mapping, f.defaultValue);
+				var v = valuePlusDominoMetaData.data;
+				delete valuePlusDominoMetaData.data;
+				metadata[f.name] = valuePlusDominoMetaData;
+
+				v = f.convert(v);
+				values[f.name] = v;
+
+			}
+			var record = new recordType(values, id);
+			record.node = n;
+			record.metadata = metadata;
+			records[records.length] = record;
+
+			record.hasChildren = n.attributes.getNamedItem('children');
+			record.isResponse = n.attributes.getNamedItem('response');
+			record.position = n.attributes.getNamedItem('position').value;
+			record.depth = record.position.split('.').length;
+			record.noteid = n.attributes.getNamedItem('noteid').value;
+			record.unid = (n.attributes.getNamedItem('unid')) ? n.attributes
+					.getNamedItem('unid').value : null;
+			record.isCategory = (record.hasChildren && !n.attributes
+					.getNamedItem('unid')) ? true : false;
+			record.isCategoryTotal = n.attributes.getNamedItem('categorytotal');
+		}
+
+		return {
+			records : records,
+			totalRecords : totalRecords || records.length
+		};
+	},
+
+	/**
+	 * Used to parse Domino's ReadViewEntries format
+	 * 
+	 * @param {Object}
+	 *            node An XML node
+	 * @param {String}
+	 *            name The name attribute to look for within the XML node
+	 * @param {String}
+	 *            defaultValue Value to return if name or node are not present
+	 * @return {String} nodeValue the value found within the XML node
+	 */
+	getViewColumnValue : function(node, map, defaultValue) {
+		var q = Ext.DomQuery;
+		var type;
+		var data;
+        var entryDataNodeMap, entryDataNode, valueDataNode;
         
-        v = f.convert(v);
-        values[f.name] = v;
-        
-      }
-      var record = new recordType(values, id);
-      record.node = n;
-      record.metadata = metadata;
-      records[records.length] = record;
-
-      record.hasChildren = n.attributes.getNamedItem('children');
-      record.isResponse = n.attributes.getNamedItem('response');
-      record.position = n.attributes.getNamedItem('position').value;
-      record.depth = record.position.split('.').length;
-      record.noteid = n.attributes.getNamedItem('noteid').value;
-      record.unid = (n.attributes.getNamedItem('unid')) ? n.attributes.getNamedItem('unid').value : null;
-      record.isCategory = (record.hasChildren && !n.attributes.getNamedItem('unid')) ? true : false;
-      record.isCategoryTotal = n.attributes.getNamedItem('categorytotal');
-    }
-
-    return {
-      records : records,
-      totalRecords : totalRecords || records.length
-    };
-  },
-
-  /**
-   * Used to parse Domino's ReadViewEntries format
-   * @param {Object} node An XML node
-   * @param {String} name The name attribute to look for within the XML node
-   * @param {String} defaultValue Value to return if name or node are not present
-   * @return {String} nodeValue the value found within the XML node
-   */
-  getViewColumnValue : function(node, colNbr, defaultValue) {
-    var q = Ext.DomQuery;
-    var type;
-    var data;
+		var oValue = {
+			type : 'text',
+			data : ''
+		};
+		
+        if (typeof map == 'number') {
+            entryDataNodeMap = "entrydata[columnnumber=" + map + "]"    
+            entryDataNode = q.select(entryDataNodeMap, node, false);
+            valueDataNode = (entryDataNode && entryDataNode[0]) ? entryDataNode[0].lastChild : false;
     
-    var oValue = {
-      type : 'text',
-      data : ''
-    };
-    var entryDataNodes = q.select('entrydata', node);
-    for (var i = 0; i < entryDataNodes.length; i++) {
-      // have to use 'columnnumber' since we can not be guaranteed that the 'name' attribute will have a value
-      var cn = q.selectNumber('@columnnumber', entryDataNodes[i]);
-
-      if (cn == colNbr) {
-
-        var lc = entryDataNodes[i].lastChild;
-
-        // now get the data
-        oValue = this.getValue(lc, lc.nodeName);
-        
-        // now get the other needed attributes
-        // category and indent are needed for categories built with the backslash
-        oValue.category = (q.selectValue('@category', entryDataNodes[i]) == 'true') ? true : false;
-        oValue.indent = (q.select('@indent', entryDataNodes[i])) ? q.selectNumber('@indent', entryDataNodes[i]) : 0;
-
-      } // end if(cn == colNbr)
-    } // end for
-
-    return oValue;
-
-  }, // end getViewColumnValue
-
-  getValue : function(node, type) {
-    var oValue = {
-      type : type,
-      //data : []
-      data : ''
-    };
-    
-    if (!node) {
-    	//oValue.data.push('');
-    	oValue.data = '';
-      
-      // now check to see if the childNodes have childNodes 
-      // if they do then this is a multi-value column
-    } else if (node.childNodes && node.childNodes.length > 0 && node.childNodes[0].hasChildNodes()) { 
-      
-      // what we are doing here is processing the values
-      // in the multi-value column
-      for (var i = 0; i < node.childNodes.length; i++) {
-        if (node.childNodes[i].firstChild.nodeName != '#text') {
-            oValue.type = node.childNodes[i].firstChild.nodeName;
         } else {
-            oValue.type = type;
+            entryDataNodeMap = map;
+            entryDataNode = q.select(entryDataNodeMap, node, false);
+            valueDataNode = (entryDataNode && entryDataNode[0]) ? entryDataNode[0] : false;
         }
         
-        /* old way was to make the the 'data' property an array and add to it
-         * however, this broke everywhere in Ext where you could process a 
-         * record such as in loadRecords(rec), etc.  Therefore, we no longer
-         * use an array and now separate the values with a newline character
-         * that we then convert to whatever the multi-value separator is for
-         * this column in the UIView dominoRenderer method
-         */
-        //oValue.data.push(node.childNodes[i].firstChild.nodeValue);
-        if (i == 0) {
-        	oValue.data = node.childNodes[i].firstChild.nodeValue;
-        } else {
-        	oValue.data += '\n' + node.childNodes[i].firstChild.nodeValue;
-        }
-      }
-    } else {
-      
-      // here is just a single value node
-      oValue.type = type;
-      
-      // if the single value node has data then it is in a childNode
-      // otherwise just send an empty string since there is not any data --- maybe better not to send an empty sring
-      if (node.childNodes && node.childNodes.length > 0) {
-        //oValue.data.push(node.childNodes[0].nodeValue);
-    	  oValue.data = node.childNodes[0].nodeValue;
-      } 
-    }
+        // sometimes valueDataNode cannot be found due to how domino sends data
+        // for instance, a category total row will only send the category
+        // total column for the very last record since this a a GRAND total
+        if (valueDataNode) {
 
-    return oValue;
+            // now get the data
+    		oValue = this.getValue(valueDataNode, valueDataNode.nodeName);
+    
+    		// now get the other needed attributes
+    		// category and indent are needed for categories built with the
+    		// backslash
+    		oValue.category = (q.selectValue('@category', entryDataNode) == 'true')
+    				? true
+    				: false;
+    		oValue.indent = (q.select('@indent', entryDataNode)) 
+                    ? q.selectNumber('@indent', entryDataNode) 
+                    : 0;
+                    
+        } // eo if (valueDataNode)    
 
-  } // end getValue
+		return oValue;
+
+	}, // end getViewColumnValue
+
+	getValue : function(node, type) {
+		var oValue = {
+			type : type,
+			// data : []
+			data : ''
+		};
+
+		if (!node) {
+			// oValue.data.push('');
+			oValue.data = '';
+
+			// now check to see if the childNodes have childNodes
+			// if they do then this is a multi-value column
+		} else if (node.childNodes && node.childNodes.length > 0
+				&& node.childNodes[0].hasChildNodes()) {
+
+			// what we are doing here is processing the values
+			// in the multi-value column
+			for (var i = 0; i < node.childNodes.length; i++) {
+				if (node.childNodes[i].firstChild.nodeName != '#text') {
+					oValue.type = node.childNodes[i].firstChild.nodeName;
+				} else {
+					oValue.type = type;
+				}
+
+				/*
+				 * old way was to make the the 'data' property an array and add
+				 * to it however, this broke everywhere in Ext where you could
+				 * process a record such as in loadRecords(rec), etc. Therefore,
+				 * we no longer use an array and now separate the values with a
+				 * newline character that we then convert to whatever the
+				 * multi-value separator is for this column in the UIView
+				 * dominoRenderer method
+				 */
+				// oValue.data.push(node.childNodes[i].firstChild.nodeValue);
+				if (i == 0) {
+					oValue.data = node.childNodes[i].firstChild.nodeValue;
+				} else {
+					oValue.data += '\n'
+							+ node.childNodes[i].firstChild.nodeValue;
+				}
+			}
+		} else {
+
+			// here is just a single value node
+			oValue.type = type;
+
+			// if the single value node has data then it is in a childNode
+			// otherwise just send an empty string since there is not any data
+			// --- maybe better not to send an empty sring
+			if (node.childNodes && node.childNodes.length > 0) {
+				// oValue.data.push(node.childNodes[0].nodeValue);
+				oValue.data = node.childNodes[0].nodeValue;
+			}
+		}
+
+		return oValue;
+
+	} // end getValue
 
 });
