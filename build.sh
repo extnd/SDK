b94d40f7d0f7317f1f1be57985494260aad2002c
Rich Waters <github@rich-waters.com>
2009-11-09 00:37:24 +0000

    fixed jsb2 & script typos

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 java16_path=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Commands/java
 jsbuilder_path=/Users/neflar/Documents/aps/JSBuilder2/JSBuilder2.jar
-jsb2_path=/Users/neflar/Documents/aps/extnd/trunk/source/tbe.jsb2
+jsb2_path=/Users/neflar/Documents/aps/extnd/trunk/source/extnd.jsb2
 output_path=/Users/neflar/Documents/aps/extnd/trunk/
 
 $java16_path -jar $jsbuilder_path --projectFile $jsb2_path --homeDir $output_path
