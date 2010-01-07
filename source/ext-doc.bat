54cab2d4479469eca0a23658b133fbaa63167a8c
Jack Ratcliff <jack.ratcliff@gmail.com>
2010-01-07 01:07:01 +0000

    change path where docs are built

--- source/ext-doc.bat
+++ source/ext-doc.bat
@@ -1,6 +1,7 @@
 set extDocJarFile="../ext-doc/ext-doc.jar"
 set extDocXMLFile="documentation.xml"
 set extDocTemplate="../ext-doc/template/ext/template.xml"
-set outputFolder="C:/Program Files/lotus/notes/data/domino/html/extnd-docs"
+set outputFolderOLD="C:/Program Files/lotus/notes/data/domino/html/extnd-docs"
+set outputFolder="build/docs"
 
 java -jar %extDocJarFile% -p %extDocXMLFile% -o %outputFolder% -t %extDocTemplate%  -verbose
