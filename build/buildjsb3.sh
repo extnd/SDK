d4c5c66f12701f65b41df0c20270a24978f96d46
Jack Ratcliff <jratcliff@sencha.com>
2012-07-10 23:12:42 -0500

    moved all build scripts to a build directory and updated extnd.jsb3 to include resources

--- build/buildjsb3.sh
+++ build/buildjsb3.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-jsbuilder=jsbuilder/JSBuilder.sh
-jsb3_path=source/extnd.jsb3
-output_path=build/
+jsbuilder=../jsbuilder/JSBuilder.sh
+jsb3_path=../build/extnd.jsb3
+output_path=../builds/
 
 $jsbuilder -p $jsb3_path -d $output_path
 
