ef6089009f02f336851bbe3e5e0d3906d3c01b5d
Jack Ratcliff <jratcliff@sencha.com>
2012-12-23 22:32:34 -0600

    updates to the build script for docs

--- build/buildDocs.sh
+++ build/buildDocs.sh
@@ -1,5 +1,11 @@
 jsduck \
   ../source \
+  ../extjs-3.4.0/src/core \
+  ../extjs-3.4.0/src/data \
+  ../extjs-3.4.0/src/dd \
+  ../extjs-3.4.0/src/state \
+  ../extjs-3.4.0/src/util \
+  ../extjs-3.4.0/src/widgets \
   -o ../builds/docs \
   --title="Extnd SDK" \
   --welcome="../doc_src/welcome.html"
