f0616661d733d13885b30c59668cf202047fdc5a
Jack Ratcliff <jratcliff@sencha.com>
2012-12-29 01:03:33 -0600

    updates to the buildDocs.sh script to copy over the docs to the ExtndDocs folder

--- build/buildDocs.sh
+++ build/buildDocs.sh
@@ -4,3 +4,6 @@ jsduck \
   -o ../builds/docs \
   --title="Extnd SDK for ExtJS 4.x" \
   --welcome="../doc_src/welcome.html"
+
+rm -rf ../../ExtndDocs/docs/docs4
+cp -R ../builds/docs ../../ExtndDocs/docs/docs4
