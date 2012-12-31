57d44df5b19b640a785e16bab793122dc758a6ef
Jack Ratcliff <jratcliff@sencha.com>
2012-12-31 17:36:12 -0600

    Removed the viewentry 'metadata' from the fields array to their own properties
    - this is so we don't clash with user defined fields of the same name

--- src/data/ViewModel.js
+++ src/data/ViewModel.js
@@ -1,88 +1,89 @@
 /**
- * Base model for Domino views.
+ * Base model for Domino views.  The #fields property is created dynamically from Ext.nd.data.ViewDesign.
  */
 Ext.define('Ext.nd.data.ViewModel', {
 
-    extend      : 'Ext.data.Model',
+    extend: 'Ext.data.Model',
 
     /**
      * @property idProperty For a Domino view we use the @position attribute since that is unique for each row
      */
-    idProperty  : 'position',
+    idProperty: 'position',
+
+
+    constructor: function(data, id, raw, convertedData) {
+        var me = this,
+            q  = Ext.DomQuery;
+
+        /**
+         * @property {Object} viewEntry Object containing the meta data Domino sends for each 'viewentry' (row)
+         * @property {String} viewEntry.position The position of this 'viewentry' in the view.  Note that Reader fields can cause 'viewentry' nodes to be missing for users not authorized.
+         * @property {String} viewEntry.unid If the row represents a document, then unid will be the UniversalID of the document.
+         * @property {String} viewEntry.noteid The NoteId of the row.  Could represent a document in the database.
+         * @property {Number} viewEntry.children Then number of immediate child documents under this document or category.
+         * @property {Number} viewEntry.descendants The total number of child documents, regardless of level, under this document or category.
+         * @property {Number} viewEntry.siblings The number of siblings in this view for the document represented by this 'viewentry' node.
+         * @property {Boolean} viewEntry.categorytotal Whether or not this is a 'category total' column.
+         * @property {Boolean} viewEntry.response Whether or not this is a response document.
+         * @property {Number} viewEntry.depth How many levels deep a category or a response document is. Used by Ext.nd.grid.ViewColumn#defaultRenderer
+         */
+        me.viewEntry = {};
+
+        Ext.apply(me.viewEntry, {
+            position        : q.selectValue('@position', raw),
+            unid            : q.selectValue('@unid', raw),
+            noteid          : q.selectValue('@noteid', raw),
+            children        : q.selectNumber('@children', raw),
+            descendants     : q.selectNumber('@descendants', raw),
+            siblings        : q.selectNumber('@siblings', raw),
+            categorytotal   : !!q.selectValue('@categorytotal', raw, false),
+            response        : !!q.selectValue('@response', raw, false)
+        });
+
+        // add a depth property used by Ext.nd.grid.ViewColumn#defaultRenderer
+        me.viewEntry.depth = me.viewEntry.position.split('.').length;
+
+        me.callParent(arguments);
+
+    },
 
     /**
-     * @property metaData
+     * Fields are created dynamically form the Ext.nd.data.ViewDesign class when it processes the ?ReadDesign and DXLExport for a view.
      */
-    metaData: {},
-
-    fields: [
-        { name: 'position',         mapping: '@position',       type: 'string'  },
-        { name: 'unid',             mapping: '@unid',           type: 'string'  },
-        { name: 'noteid',           mapping: '@noteid',         type: 'string'  },
-        {
-            name    : 'children',
-            type    : 'number',
-            convert : function (v, rec) {
-                var children;
-                // we handle the @children attribute ourselves since Ext.DomQuery.selectNode returns children nodes
-                // instead of returning the @children 'attribute'
-                children = rec.raw.attributes.getNamedItem('children');
-                return children ? parseFloat(children.nodeValue, 0) : 0;
-            }
-        },
-        { name: 'descendants',      mapping: '@descendants',    type: 'number'  },
-        { name: 'siblings',         mapping: '@siblings',       type: 'number'  },
-        { name: 'categorytotal',    mapping: '@categorytotal',  type: 'number'  },
-        {
-            name    : 'response',
-            mapping : '@response',
-            type    : 'boolean',
-            convert : function (v, rec) {
-                return (v === 'TRUE') ? true : false;
-            }
-        },
-        {
-            name    : 'depth',
-            type    : 'int',
-            convert : function (v, rec) {
-                return rec.get('position').split('.').length;
-            }
-        }
-
-    ],
+    fields: [],
 
 
     /**
      * If the viewentry/record has children
-     * @return {boolean}
+     * @return {Boolean}
      */
     hasChildren: function () {
-        return !!this.get('children');
+        return !!this.viewEntry.children;
     },
 
     /**
      * If the viewentry/record is a response
-     * @return {boolean}
+     * @return {Boolean}
      */
     isResponse: function () {
-        return !!this.get('response');
+        return this.viewEntry.response;
     },
 
     /**
      * If the viewentry/record is a category
-     * @return {boolean}
+     * @return {Boolean}
      */
     isCategory: function () {
         var me = this;
-        return (me.hasChildren() && !me.get('unid')) ? true : false;
+        return (me.hasChildren() && !me.viewEntry.unid) ? true : false;
     },
 
     /**
      * If the viewentry/record is a category total
-     * @return {boolean}
+     * @return {Boolean}
      */
     isCategoryTotal: function () {
-        return !!this.get('categorytotal');
+        return this.viewEntry.categorytotal;
     }
 
 });
