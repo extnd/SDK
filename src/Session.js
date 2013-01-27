9df52d7e08fd0fe9e2e131aec84d701fe7b5ecd3
Jack Ratcliff <jratcliff@sencha.com>
2013-01-27 00:03:22 -0600

    Work on the Extnd.Session class

--- src/Session.js
+++ src/Session.js
@@ -1,92 +1,136 @@
 /**
  * @class Extnd.Session
- * @singleton
  */
 Ext.define('Extnd.Session', {
 
-    singleton: true,
+    /**
+     * @property {Extnd.Database[]} addressBooks
+     * The Domino Directories and Personal Address Books, including directory catalogs, known to the current session.
+     */
 
     /**
-     * common user name
-     * example: "Rich Waters"
-     * @type {String}
+     * @property {String} commonUserName
+     * The common name portion of the current user's name.
      */
-    commonUserName : "Anonymous",
+
     /**
-     * effective user name
-     * example: "CN=Rich Waters/O=ExtND"
-     * @type {String}
+     * @property {Extnd.Database} currentDatabase
+     * The database first accessed when this session was initialized.  If no database was specified, then this will be the Extnd database.
      */
-    effectiveUserName : "Anonymous",
+
     /**
-     * notes build version
-     * @type {Integer}
+     * @property {String} effectiveUserName
+     * The user name that is in effect for the current program.
      */
-    notesBuildVersion : 261,
+
     /**
-     * notes version
-     * example: "Release 7.0.1|January 17, 2006"
-     * @type {String}
+     * @property {String} httpURL
+     * The Domino URL of a session when HTTP protocols are in effect.
      */
-    notesVersion : "Release 7.0.1|January 17, 2006",
+
     /**
-     * platform the server is running on
-     * @type {String}
+     * @property {Float} notesBuildVersion
+     * The Domino server's release number.
      */
-    platform : "Windows/32",
+
     /**
-     * server name
-     * @type {String}
+     * @property {String} notesVersion
+     * The Domino server's release version.  Example: "Release 7.0.1|January 17, 2006"
      */
-    serverName : "CN=Web/O=BlueCuda",
+
     /**
-     * list of groups current user is a member of
+     * @property {String} platform
+     * The name of the platform the server is running on.
+     */
+
+    /**
+     * @property {String} serverName
+     * The full name of the server that the session is running on.
+     */
+
+    /**
+     * @property {Extnd.NotesName[]} userGroupNameList
+     * The groups to which the current user belongs.
      * example: {"abbreviated" : "ExtNDAdmin", "addr821" : "", "common" : "ExtNDAdmin"}
-     * @type {Array}
      */
-    userGroupNameList : [{
-        "abbreviated" : "*/ExtND",
-        "addr821" : "",
-        "common" : "*"
-    }, {
-        "abbreviated" : "ExtNDAdmin",
-        "addr821" : "",
-        "common" : "ExtNDAdmin"
-    }],
+
     /**
-     * current user name
+     * @property {String} userName
+     * The current user's name.
      * example: "CN=Rich Waters/O=ExtND"
-     * @type {String}
      */
-    userName : "CN=Rich Waters/O=ExtND",
+
     /**
-     * user name list array
-     * @type {Array}
+     * @property {String[]} userNameList
+     * The name of the user that created the session, and the alternate name if it exists.
      */
-    userNameList : [{
-        "abbreviated" : "CN=Rich Waters/O=ExtND",
-        "addr821" : "CN=Rich Waters/O=ExtND",
-        "common" : "CN=Rich Waters/O=ExtND"
-    }, {
-        "abbreviated" : "CN=Web/O=BlueCuda",
-        "addr821" : "CN=Web/O=BlueCuda",
-        "common" : "CN=Web/O=BlueCuda"
-    }],
+
     /**
      * user roles
      * @type {Array}
      */
-    userRoles : [""],
+
+
+    constructor: function (config) {
+        var me = this;
+
+        Ext.apply(me, config);
+
+        Ext.Ajax.request({
+            method          : 'GET',
+            disableCaching  : true,
+            success         : me.handleSuccess,
+            failure         : me.handleFailure,
+            scope           : me,
+            options         : config,
+            url             : me.extndUrl + 'Session.json?OpenAgent&db=' + me.dbPath || ''
+        });
+
+
+    },
+
+    handleSuccess: function (response, request) {
+        var sessionData = Ext.decode(response.responseText),
+            options     = request.options;
+
+        // apply sessionData to this
+        Ext.apply(this, sessionData);
+
+        // now call the callback and pass the session (this) to it
+        Ext.callback(options.success, options.scope, [this]);
+    },
+
+    handleFailure: function (response) {
+        console.log('failed');
+    },
+
     /**
-     * Array of {@link Ext.nd.Database} objects pertaining to the
-     * available address books
-     * @type {Array}
+     * Evaluates a Domino formula.
      */
-    addressBooks : [],
+    evaluate: function (formula, doc) {
+        console.log('TODO implement evaluate code');
+    },
+
     /**
-     * Current {@link Ext.nd.Database} object
-     * @type {Ext.nd.Database}
+     * Creates an Extnd.Database object that represents the database located at the server and file name you specify,
+     * and opens the database, if possible.
      */
-    currentDatabase : null
+    getDatabase: function (server, dbFile, createOnFail) {
+        console.log('TODO implement getDatabase code');
+    },
+
+    /**
+     * Creates a New Extnd.DbDirectory object using the name of the server you wnat to access.
+     */
+    getDbDirectory: function (server) {
+        console.log('TODO implement getDbDirectory');
+    },
+
+    /**
+     * Creates a New Extnd.Directory object using the name of the server you wnat to access.
+     */
+    getDirectory: function (server) {
+        console.log('TODO implement getDirectory');
+    }
 
 });
