e9ca1ab16b6b7715d726213a619114633a2845fb
Jack Ratcliff <jratcliff@sencha.com>
2013-04-07 21:54:15 -0500

    Adding the index.js for the examples db

---
+++ database/examples/src/index.js
@@ -0,0 +1,42 @@
+﻿Ext.Loader.setConfig({
+    enabled         : true,
+    disableCaching  : false,
+    paths: {
+        'Ext'   : '../extnd.nsf/extjs/src',
+        'Extnd' : '../extnd.nsf/extnd/src'
+    }
+});
+
+Ext.onReady(function () {
+    var westPanel,
+        centerPanel;
+
+    centerPanel = Ext.create('Ext.TabPanel', {
+        region      : 'center',
+        margins     : '5 5 5 0',
+        activeItem  : 0,
+        items: [{
+            title           : 'Home',
+            padding         : 10,
+            contentEl       : 'home',
+            styleHtmlContent: true,
+            closable        : false
+        }]
+    });
+
+    westPanel = Ext.create('Extnd.UIOutline', {
+        region      : 'west',
+        margins     : '5 5 5 5',
+        title       : 'Extnd Examples Explorer',
+        collapsible : true,
+        outlineName : 'mainOL',
+        target      : centerPanel,
+        width       : 250
+    });
+
+    Ext.create('Ext.Viewport', {
+        layout: 'border',
+        items: [westPanel, centerPanel]
+    });
+
+});
